cef371f6b20ac02e5e9593c4fcbc3aea6171bae5
jas7o <jasio2k00@gmail.com>
2022-09-09 01:22:02 +0100

    beyond2lte: adapt to crDroid Maintainter: override

--- lineage_beyond2lte.mk
+++ lineage_beyond2lte.mk
@@ -1,42 +1,32 @@
 #
-# Copyright (C) 2021 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright (C) 2022 The LineageOS Project
 #
 
 ## Inherit from generic products, most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-## Product API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-## Inscreen Fingerprint HAL
-TARGET_HAVE_FOD := true
+## Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Inherit from beyond2lte device
 $(call inherit-product, device/samsung/beyond2lte/device.mk)
 
-## Boot Animation
+## Inherit some common build stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+
+## Flags
 TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 3040
 TARGET_SCREEN_WIDTH := 1440
-
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-## Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+TARGET_HAVE_FOD := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+TARGET_USES_AOSP_RECOVERY := true
 
 ## Device identifier, this must come after all inclusions
 PRODUCT_NAME := lineage_beyond2lte
@@ -45,4 +35,8 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G975F
 PRODUCT_MANUFACTURER := samsung
 
+# Maintainter
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.maintainer.name=jas7o
+    
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
